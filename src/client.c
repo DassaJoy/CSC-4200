4609f45453a2a9101fc6364ee6609cab368358fc
DassaJoy <hrjoy2003@gmail.com>
2025-03-15 12:01:11 -0500

    updated comments

--- src/client.c
+++ src/client.c
@@ -1,12 +1,13 @@
 // All Libraries
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> // allows the program to use functions from the Standard Input Output (stdio) Library
+#include <unistd.h> // essentail in Unix/Linux-based systems and provides access to the Portable Operating Sysstem Interface API (commonly used for system calls related to process control, file operations, and system interaction)
+#include <stdlib.h> // provides functions for memory management, process control, conversions, and other general-purpose utilites (part of the Standard Library)
+#include <string.h> // provides functions for manipulating and handling C-style strings (essential when working with text-based in C)
 #include <arpa/inet.h> // converts IP addresses between text and binary format; converts values between host and network byte order
 #include <openssl/ssl.h> // provides cryptographic functions and Secure Sockets Layer / Transport Layer Security
 #include <openssl/err.h> // provides functions for handling and repoting OpenSSl errors
 
+// Symbolic Constants
 #define SERVER_IP "127.0.0.1"
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -57,21 +58,26 @@ SSL_CTX *create()
     return ctx;
 }
 
-//Function that communicates with the server
+// Function that communicates with the server
+/*
+    Handles user input, sends it to the server via SSL
+    and waits for and prints the server's response
+*/
 void communicate_server(SSL *sl)
 {
     char size[BUFFER_SIZE];
 
-    //This gets user's input
+    // This gets user's input
     printf("Enter message: ");
     fgets(size, sizeof(size), stdin);
 
-    //This sends the message to server
+    // This sends the message to server
     SSL_write(sl, size, strlen(size));
 
-    //Reads the server's response
+    // Reads the server's response
     int byte = SSL_read(sl, size, sizeof(size));
 
+    // Checks if SSL_read failed
     if(byte <= 0)
     {
         perror("SSL read has failed");
@@ -85,38 +91,39 @@ void communicate_server(SSL *sl)
 
 int main()
 {
-    //Varibales
+    // Varibales
     int sock;
     struct sockaddr_in add;
     SSL_CTX *ctx;
     SSL *sl;
 
-    //Launches OpenSSl
+    // Launches OpenSSl
     start_openssl();
     ctx = create();
 
-    //Creates the socket
+    // Creates the socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Checks to see if the creation of socket worked
     if(sock < 0)
     {
         perror("Socket creation has failed");
         exit(EXIT_FAILURE);
     }
 
-    //sockaddr_in fields
-    add.sin_family = AF_INET;
-    add.sin_port = htons(PORT);
-    add.sin_addr.s_addr = inet_addr(SERVER_IP);
+    // sockaddr_in fields
+    add.sin_family = AF_INET; // specifies the address family 
+    add.sin_port = htons(PORT); // stores the port number, must be in network byte order
+    add.sin_addr.s_addr = inet_addr(SERVER_IP); //stores the IPv4 address, in network byte order
 
-    //Connection to the server
+    // Checks connection to the server
     if(connect(sock, (struct sockaddr*)&add, sizeof(add)) < 0)
     {
         perror("Connection has failed");
         exit(EXIT_FAILURE);
     }
 
-    //Creates SSL object
+    // Creates SSL object
     sl = SSL_new(ctx);
     SSL_set_fd(sl, sock);
 
@@ -134,6 +141,7 @@ int main()
         communicate_server(sl);
     }
 
+    // Clean up
     close(sock);
     SSL_free(sl);
     clean_openssl();
--- src/server.c
+++ src/server.c
@@ -1,34 +1,42 @@
 //All Libraries
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdio.h> // allows the program to use functions from the Standard Input Output (stdio) Library
+#include <stdlib.h> // essentail in Unix/Linux-based systems and provides access to the Portable Operating Sysstem Interface API (commonly used for system calls related to process control, file operations, and system interaction)
+#include <string.h> // provides functions for memory management, process control, conversions, and other general-purpose utilites (part of the Standard Library)
+#include <unistd.h> // provides functions for manipulating and handling C-style strings (essential when working with text-based in C)
 #include <arpa/inet.h> // converts IP addresses between text and binary format; converts values between host and network byte order
 #include <openssl/ssl.h> // provides cryptographic functions and Secure Sockets Layer / Transport Layer Security
 #include <openssl/err.h> // provides functions for handling and repoting OpenSSl errors
-#include <pthread.h>
+#include <pthread.h> // includes the POSIX Threads library, which provides functions for creating and managing multi-threaded programs
 
+// Synbolic Constants
 #define PORT 8080
 #define MAX_CLIENTS 5
 #define BUFFER_SIZE 1024
 #define LOG_FILE "server.log"
 
-FILE *log_file;
-pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+FILE *log_file; // Declares a file pointer (DOES NOT OPEN A FILE)
+pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER; // Initializes a mutex for thread synchronization
 
 //Function to log messages to a file
+/*
+    Logs a message to a file safely in a multi-threaded environment
+    The function locks a mutex to ensure thread-safe access to the log file.
+    It opens the log file in append mode, and writes the message, then closes 
+    the file. It unlocks the mutex to allow other threads to log messages.
+*/
 void log_message(const char *mess)
 {
-    pthread_mutex_lock(&log_mutex);
-    log_file = fopen(LOG_FILE, "a");
+    pthread_mutex_lock(&log_mutex); // lock the mutex to prevent concurrent file access from multiple threads
+    log_file = fopen(LOG_FILE, "a"); // open the log file in append mode
 
+    // checks to see if file was opened
     if (log_file)
     {
         fprintf(log_file, "%s\n", mess);
         fclose(log_file);
     }
 
-    pthread_mutex_unlock(&log_mutex);
+    pthread_mutex_unlock(&log_mutex); // Unlocks the mutex to allow other threads to log messages
 }
 
 //Function to start openssl
@@ -77,8 +85,14 @@ SSL_CTX *create()
 }
 
 //Function that configures SSL
+/*
+    Loads an SSL certificate from a specified file.
+    Loads the corresponding privaate key. 
+    Verifies that the private key matches the certificate
+*/
 void configure(SSL_CTX *ctx)
 {
+    // Load the SLL certificate from file
     if(SSL_CTX_use_certificate_file(ctx, "ssl_certs/server.crt", SSL_FILETYPE_PEM) <= 0)
     {
         log_message("Unable to load the certificate file");
@@ -86,6 +100,7 @@ void configure(SSL_CTX *ctx)
         exit(EXIT_FAILURE);
     }
 
+    // Load the private key from the file
     if(SSL_CTX_use_PrivateKey_file(ctx, "ssl_certs/server.key", SSL_FILETYPE_PEM) <= 0)
     {
         log_message("Unable to load the private key file");
@@ -93,6 +108,7 @@ void configure(SSL_CTX *ctx)
         exit(EXIT_FAILURE);
     }
 
+    // Verify that the loaded private key matches the loaded certificate
     if(!SSL_CTX_check_private_key(ctx))
     {
         log_message("Private key does not match the public certificate");
@@ -101,6 +117,11 @@ void configure(SSL_CTX *ctx)
 }
 
 //Function that handles the client communication
+/*
+    Handles receiving a message from the client, logging
+    the recieved message and sending a confirmation message
+    back to the client.
+*/
 void *client(void *sl)
 {
     //Varibles
@@ -111,6 +132,7 @@ void *client(void *sl)
     //Receives message from client
     byte = SSL_read(new_sl, size, sizeof(size));
 
+    // Check if the read operation works
     if(byte <= 0)
     {
         log_message("SSL read has failed");
@@ -149,15 +171,16 @@ int main()
     //Creates the socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Checks to see if the creation of the socket worked
     if(sock < 0)
      {
         perror("Socket creation has failed");
         exit(EXIT_FAILURE);
     }
     
-    add.sin_family = AF_INET;
-    add.sin_addr.s_addr = INADDR_ANY;
-    add.sin_port = htons(PORT);
+    add.sin_family = AF_INET; // specifies the address family 
+    add.sin_addr.s_addr = INADDR_ANY; // stores the port number, must be in network byte order
+    add.sin_port = htons(PORT); //stores the IPv4 address, in network byte order
 
     //Binds the socket
     if(bind(sock, (struct sockaddr*)&add, sizeof(add)) < 0)
@@ -183,6 +206,7 @@ int main()
     {
         cl_sock = accept(sock, (struct sockaddr*)&cl_add, &cl_len);
         
+        // Checks if cl_sock failed
         if(cl_sock < 0)
         {
             perror("Accpeting has failed");
